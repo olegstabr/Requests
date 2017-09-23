a795d93373aae708fcbb66ed8f017f625655c447
olegstabr <olegstabr@gmail.com>
2017-09-23 20:40:40 +0700

    Send requests from queue

--- src/com/company/Client/Client.java
+++ src/com/company/Client/Client.java
@@ -5,9 +5,11 @@ import com.company.Data.Request;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Client {
     private final int PORT = 7777;
@@ -15,9 +17,10 @@ public class Client {
     private static volatile Client instance;
 
     private Socket clientSocket = null;
-    private List<Request> queue = new ArrayList<>();
+    private List<Request> queue = Collections.synchronizedList(new ArrayList<>());
+    private Object lockObject = new Object();
 
-    public Client() {
+    Client() {
         try {
             clientSocket = new Socket(IP, PORT);
         } catch (IOException e) {
@@ -25,32 +28,54 @@ public class Client {
         }
     }
 
-    public void send(byte[] data) {
+    public void send() {
+        ReentrantLock locker = new ReentrantLock();
         try {
-            BufferedOutputStream outputStream = new BufferedOutputStream(clientSocket.getOutputStream());
-            outputStream.write(data);
-            outputStream.flush();
+            boolean lockFlag = locker.tryLock(100, TimeUnit.MILLISECONDS);
+            if (lockFlag) {
+                try {
+                    while (!queue.isEmpty()) {
+                        int queueLength = queue.size();
+                        Request request = queue.get(queueLength - 1);
+                        byte[] data = request.getBytes();
+                        int value = request.getId();
 
-            ByteBuffer byteBuffer = ByteBuffer.wrap(data);
-            int value = byteBuffer.getInt();
+                        BufferedOutputStream outputStream = new BufferedOutputStream(clientSocket.getOutputStream());
+                        outputStream.write(data);
+                        outputStream.flush();
 
-            System.out.println("send " + value);
+                        queue.remove(request);
+                        System.out.println("send " + value);
+                    }
+                } finally {
+                    locker.unlock();
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void updateQueue(Request request) {
-        int queueSize = queue.size();
-        request.setId(queueSize == 0 ? 0 : queueSize - 1);
-        queue.add(request);
+    void updateQueue(Request request) {
+        ReentrantLock locker = new ReentrantLock();
+        locker.lock();
+        try {
+            int queueSize = queue.size();
+            request.setId(queueSize == 0 ? 0 : queueSize - 1);
+            queue.add(request);
+        } finally {
+            locker.unlock();
+        }
+
     }
 
 
     //
     // Singleton
     //
-    public static Client getInstance() {
+    static Client getInstance() {
         Client localInstance = instance;
         if (localInstance == null) {
             synchronized (Client.class) {
