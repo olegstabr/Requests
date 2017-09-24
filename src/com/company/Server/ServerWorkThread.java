32fc26fcf6fff84fdb36e01113e14b744f053b90
olegstabr <olegstabr@gmail.com>
2017-09-24 15:59:32 +0700

    SERVER: Refactor code

--- src/com/company/Server/ServerWorkThread.java
+++ src/com/company/Server/ServerWorkThread.java
@@ -4,6 +4,7 @@ import com.company.Data.Request;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 
@@ -32,14 +33,7 @@ public class ServerWorkThread extends Thread {
                     break;
                 }
 
-                ByteBuffer byteBuffer = ByteBuffer.wrap(data);
-                int requestId = byteBuffer.getInt();
-                int messageLength = byteBuffer.getInt();
-                byte[] messageBytes = new byte[messageLength];
-                byteBuffer.get(messageBytes, 0, messageLength);
-                String message = new String(messageBytes, "UTF-8");
-
-                Request request = new Request(requestId, message);
+                Request request = decodeMessage(data);
                 System.out.println(clientSocket.getInetAddress() + ":" + clientSocket.getPort() + " | request = " + request.toString());
             }
             bufferedInputStream.close();
@@ -47,5 +41,22 @@ public class ServerWorkThread extends Thread {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private Request decodeMessage(byte[] data) {
+        Request request = null;
+        try {
+            ByteBuffer byteBuffer = ByteBuffer.wrap(data);
+            int requestId = byteBuffer.getInt();
+            int messageLength = byteBuffer.getInt();
+            byte[] messageBytes = new byte[messageLength];
+            byteBuffer.get(messageBytes, 0, messageLength);
+            String message = new String(messageBytes, "UTF-8");
+
+            request = new Request(requestId, message);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return request;
     }
 }
